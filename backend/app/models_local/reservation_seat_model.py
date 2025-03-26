3148286f966af031192fe29d7cbf4724542124aa
Lukasz Filo <lukif02@gmail.com>
2025-03-26 17:10:53 +0100

    fixed db relations

--- backend/app/models_local/reservation_seat_model.py
+++ backend/app/models_local/reservation_seat_model.py
@@ -7,6 +7,7 @@ class Reservation_Seat(LocalBase):
     __tablename__ = 'reservation_seats'
 
     id = Column(Integer, primary_key=True, index=True)
+    seat_id = Column(Integer, ForeignKey('seats.id'), index=True)
     reservation_id = Column(Integer, ForeignKey('reservations.id'), index=True)
 
     reservation = relationship("Reservation", back_populates="reservation_seat")
